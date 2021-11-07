240096e819403943029e0393e4db8c7e04c00e62
teerawat prarom <teewat@yahoo.com>
2021-11-07 20:36:21 +1100

    completed assignment

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+    programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -36,7 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -82,6 +84,9 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP","C"]}
+    programmer_hash
+
 end
 
 def changing_alan
@@ -103,6 +108,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 
 end
